cad59bb8233485bf1bd1d24723fd1029b319a0a5
Jay Rathod <Jay@Jays-MacBook-Pro.local>
2020-10-02 23:49:11 +0400

    Updates

---
+++ precache-manifest.a0ddf1c7286027c13c8dd9b9347196a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d50449460222f03441b8a59796710ae8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71aecc97ba52878176c1",
+    "url": "/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "9d1c739a863b1874a364",
+    "url": "/static/js/2.d6d80f44.chunk.js"
+  },
+  {
+    "revision": "320addaff3e37295a01ab28fa4b66477",
+    "url": "/static/js/2.d6d80f44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71aecc97ba52878176c1",
+    "url": "/static/js/main.bb002b22.chunk.js"
+  },
+  {
+    "revision": "dda15dd46040c911a58a",
+    "url": "/static/js/runtime-main.d2c213fc.js"
+  }
+]);
